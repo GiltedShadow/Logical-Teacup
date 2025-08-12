d3598544ce26b423d2fe5fef69139c741a29c1fa
Alex Norment <87099499+GiltedShadow@users.noreply.github.com>
2025-08-12 16:04:36 -0700

    stage one mostly complete
    stage one running and fully functional as is, does everything out of the box
    Will need to adjust/ add functionality based off of stage 2 and 3 usage - already found something to adjust  and will need to add several checks and functions to it T.T

--- Python psudo coding/psudoMainConfig.py
+++ Python psudo coding/psudoMainConfig.py
@@ -82,13 +82,21 @@ import sqlite3
 import os
 import logging
 
-fileVersion = 0.1
+fileVersion = 0.2
+# 0.1 added full functionality of the plant tracking and user information based off of .csv files
+# 0.2 adding watering function based off of information input by the user
+# 0.3 adding air control based off of information input by the user and external temperature & humidity readings
 
+
+######################################################################################
+#Stage one - user information storage
+######################################################################################
 userPresetsFile = "UserPresets.csv"
 plantsFile = "plantList.csv"
 databaseFile = "test.db"
 
-userFieldnames = ['Name', 'Watering Style', 'Ventilation', 'Panels', 'Frame']
+#TODO add spots available
+userFieldnames = ['Name', 'Spots Available', 'Watering Style', 'Ventilation', 'Panels', 'Frame']
 userReseponses = []
 plantFieldnames = ['Plant', 'Placement', 'Pot Style', 'Moisture' ,'Temperature', 'Humidity']
 plantResponses = []
@@ -187,7 +195,7 @@ def user_settings():
     pull_user_prefrences()
 
 def plant_settings():
-
+#TODO add a check to make sure that plants are not added to spots above "spots avaialable" found in the user presets file
     pull_plant_list()
     for plant in plantListList:
         print(plant)
@@ -237,7 +245,7 @@ def plant_settings():
                 except:
                     logging.warning("Attempted to enter a non int non quit value to adjust plants, entry " + plantToModify + ", continuing")
                     continue
-
+                if plantToModify == 0:print("please do not attempt to adjust the column names");continue
                 for item in plantFieldnames:
                     userResponse = input(item + "?\n>>")
                     if userResponse == 'q' or userResponse == 'quit':
@@ -247,32 +255,32 @@ def plant_settings():
                     plantResponses.append(userResponse)
                 logging.debug(str(plantListList[int(plantToModify)]) + " adjusted to " + str(plantResponses))
                 print(plantResponses)
-                for plant in plantListList:
-                    print(plant)
-                input("press any key to continue")
+                # input("press any key to continue")
+                # for plant in plantListList:
+                #     print(plant)
+                # input("press any key to continue")
                 plantListList[int(plantToModify)] = plantResponses
-                for plant in plantListList:
-                    print(plant)
+                # for plant in plantListList:
+                #     print(plant)
                 with open(plantsFile, 'w', newline='') as csvfile:
-                        plantOverwriter = csv.DictWriter(csvfile, fieldnames=plantFieldnames)
-                        plantOverwriter.writeheader
+                    plantOverwriter = csv.DictWriter(csvfile, fieldnames=plantFieldnames)
+                    plantOverwriter.writeheader
                 for row in plantListList:
-                        with open(plantsFile, 'a', newline='') as csvfile:
-                            plantWriter = csv.DictWriter(csvfile, fieldnames=plantFieldnames)
-                            plantWriter.writerow({'Plant':row[0], 'Placement':row[1], 
-                            'Pot Style':row[2],'Moisture':row[3], 
-                            'Temperature':row[4], 'Humidity':row[5]})
-                        # print({'Plant':row[0], 'Placement':row[1], 
-                        #     'Pot Style':row[2],'Moisture':row[3], 
-                        #     'Temperature':row[4], 'Humidity':row[5]})
+                    with open(plantsFile, 'a', newline='') as csvfile:
+                        plantWriter = csv.DictWriter(csvfile, fieldnames=plantFieldnames)
+                        plantWriter.writerow({'Plant':row[0], 'Placement':row[1], 
+                        'Pot Style':row[2],'Moisture':row[3], 
+                        'Temperature':row[4], 'Humidity':row[5]})
+                    # print({'Plant':row[0], 'Placement':row[1], 
+                    #     'Pot Style':row[2],'Moisture':row[3], 
+                    #     'Temperature':row[4], 'Humidity':row[5]})
             #0'plant', 1'placement', 2'pot style', 3'moisture', 4'temperature', 5'humidity'
                         print("Plants adjusted")
-                        plantResponses.clear()
+                plantResponses.clear()
             
             return print("plants adjusted") #overwrite whole file with adjusted information
         
         case 'delete'|'del'|'d':
-            #TODO confirm functionality of delete and active loop
             active=True
             while active:
                 pull_plant_list()
@@ -289,6 +297,7 @@ def plant_settings():
                 except:
                     logging.warning("Attempted to enter a non int non quit value to delete plants, entry " + plantToModify + ", continuing")
                     continue
+                if plantToModify == 0:print("please do not attempt to remove the column names");continue
                 logging.debug(str(plantListList[int(plantToModify)]) + " deleted")
                 plantListList.pop(int(plantToModify))
                 with open(plantsFile, 'w', newline='') as csvfile:
@@ -319,15 +328,82 @@ def pull_user_prefrences():
         reader = csv.DictReader(csvfile)
         for row in reader:
             userPrefList.append(row)
+######################################################################################
+#Stage one - user information storage
+######################################################################################
+
+######################################################################################
+#Stage two - watering logic
+######################################################################################
+#arggghhh what do i even do here
+#how often should the watering be run?
+#   maybe check with user, should be an overall setting or perhaps a per plant setting rather than keeping the plants at a specific moisture
+#   to start, plants should be watered once a day
+#how will the logic know how much to water
+#   there should be a feedback loop here - first is a guess say 30 seconds of watering to start
+#   based on the next days reading for the moisture, the water should be adjusted
+#   once the moisture reading can be steady at the setting set by the plant list entry, level off
+#moisture measurements should be taken hourly? quarterly? user set measurement
+#   there should be a way to not have the moisture measurements, like users taking manual measurements
+#how will long pots be categorized? should they be 1a, 1b, etc or be classified as separate spots 1, 2, etc
+#   these spts should have some form of averaged moisture content 
+#TODO take measurements with moisture sensor, water based on a table, log based on plant spot vs moisture at time of watering
+#TODO take measurements with moisture sendor, off of the watering schedule to check against drainage, if moisture reads around the requested amount it gets a carrot
+#   this should be in between the waterings and should assist with the watering schedule and amount
+plantSpotsTurnedOn = []
+
+def turn_on_off_spots():
+    #TODO pull plant spots from plant CSV
+    pass
+
+def take_moisture():
+    #TODO function to take moisture measurements
+    # This needs to search for moisture sensors, or for lack of sensors (mostly for this code or for when users do not have them) assume a specific loss based on plant spots
+    # this needs to average long pots to one value over multiple sensors with error checing and checks to see if one spot's watering spout is encountering an issue
+    for plant in plantSpotsTurnedOn:
+        print("moisture collected for " + plant)
+    pass
+
+
+
+def water_plants():
+    #TODO function to use moisture to water
+    # this will pull the moisture based on each plant placement (see moisture function) potentially based on history and previous moisture measurements and will water based on time
+    pass
+
+
+
+def adjust_water():
+    #TODO function to use moisture to check water
+    # this will pull moisture (see moisture function) and the previous water amount (see water function) to adjust if the time needs to be increased or decreased
+    pass
+
+
+#TODO function to adjust water values based off of first 3
+# this may be integrated into the watering functionv
+#remember, it is ok to overwater --- but only a little bit, dont kill plants
+
+######################################################################################
+#Stage two - watering logic
+######################################################################################
+
+
+######################################################################################
+#Stage three - air control logic
+######################################################################################
+#   
+######################################################################################
+#Stage three - air control logic
+######################################################################################
 
 print(introText)
 logging.info("starting program")
 file_check()
 pull_plant_list()
 pull_user_prefrences()
-print(userPrefList)
-for row in plantListList:
-    print(row)
+# print(userPrefList)
+# for row in plantListList:
+#     print(row)
 while(True):
     # main menu!
     #should add the user settings and plant list when user logs in, this should make the try catch loops much easier
